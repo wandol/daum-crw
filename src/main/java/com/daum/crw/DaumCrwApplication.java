b83333665f23b9e8460b9a6a1244f6c63c32f05f
wandol <wandol@kakao.com>
2020-11-09 19:47:18 +0900

    init

---
+++ src/main/java/com/daum/crw/DaumCrwApplication.java
@@ -0,0 +1,13 @@
+package com.daum.crw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaumCrwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaumCrwApplication.class, args);
+	}
+
+}
